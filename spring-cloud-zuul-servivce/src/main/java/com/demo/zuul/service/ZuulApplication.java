ed65ced7c9efc7e85f60f8e5440ab7ebfddb0dee
wch <527622102@qq.com>
2020-06-08 11:00:37 +0800

    集成zuul

---
+++ spring-cloud-zuul-servivce/src/main/java/com/demo/zuul/service/ZuulApplication.java
@@ -0,0 +1,35 @@
+package com.demo.zuul.service;
+
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @Author: wch
+ * @Description:
+ * @Date: 2020/6/6 11:52 PM
+ */
+@EnableZuulProxy
+@SpringBootApplication
+@EnableHystrixDashboard
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);  //系统启动时加载顺序
+        registrationBean.addUrlMappings("/hystrix.stream");//路径
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
